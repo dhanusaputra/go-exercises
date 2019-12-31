7b8e091272c9a527761bc5e60935a5296e969bd6
dhanu <andreasdhanu@gmail.com>
2019-12-31 19:36:12 +0700

    fix convert float

--- euler/powerDigitSum/main.go
+++ euler/powerDigitSum/main.go
@@ -10,14 +10,14 @@ import (
 
 func main() {
 	n := flag.Float64("n", 2, "number")
-	e := flag.Float64("e", 10, "exponent")
+	e := flag.Float64("e", 1000, "exponent")
 	flag.Parse()
 	fmt.Println(getPowerDigitSum(*n, *e))
 }
 
 func getPowerDigitSum(n float64, e float64) int {
 	var res int
-	for _, i := range strings.Split(fmt.Sprintf("%.f", math.Pow(n, e)), "") {
+	for _, i := range strings.Split(strconv.FormatFloat(math.Pow(n, e), 'f', 0, 64), "") {
 		tmp, err := strconv.Atoi(i)
 		if err != nil {
 			panic(err)
