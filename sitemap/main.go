75da11d3a261111481a5a70298d5385a522a69ea
dhanu <andreasdhanu@gmail.com>
2020-01-14 08:14:24 +0700

    add bfs

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -12,12 +13,44 @@ import (
 
 func main() {
 	URLFlag := flag.String("url", "https://gophercises.com", "the url that you  want to build")
+	maxDepth := flag.Int("depth", 10, "the maximum number of links deep")
 	flag.Parse()
 
-	fmt.Println(*URLFlag)
-	resp, err := http.Get(*URLFlag)
+	pages := bfs(*URLFlag, *maxDepth)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func bfs(URLStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		URLStr: struct{}{},
+	}
+	for i := 0; i < maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		for url := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
+func get(URLStr string) []string {
+	resp, err := http.Get(URLStr)
 	if err != nil {
-		panic(err)
+		return []string{}
 	}
 	defer resp.Body.Close()
 
@@ -28,18 +61,36 @@ func main() {
 	}
 	base := baseURL.String()
 
-	links, _ := link.Parse(resp.Body)
-	var hrefs []string
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var ret []string
 	for _, l := range links {
 		switch {
 		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base+l.Href)
+			ret = append(ret, base+l.Href)
 		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)
+			ret = append(ret, l.Href)
 		}
 	}
-	for _, href := range hrefs {
-		fmt.Println(href)
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
 	}
 }
 
