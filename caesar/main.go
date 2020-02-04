2304285cafcbc15409b457eb21fb4fa1e7dc4dbd
dhanu <andreasdhanu@gmail.com>
2020-02-04 08:09:10 +0700

    add caesar

---
+++ caesar/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var length, delta int
+	var input string
+	fmt.Scanf("%d\n", &length)
+	fmt.Scanf("%s\n", &input)
+	fmt.Scanf("%d\n", &delta)
+
+	fmt.Printf("%d\n", length)
+	fmt.Printf("%s\n", input)
+	fmt.Printf("%d\n", delta)
+}
