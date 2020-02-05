09cd8d133fc95ef5ef063db7a135c59c559969ff
dhanu <andreasdhanu@gmail.com>
2020-02-05 07:47:04 +0700

    fix

--- caesar/main.go
+++ caesar/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+import "strings"
+
 func main() {
 	var length, delta int
 	var input string
@@ -9,7 +11,27 @@ func main() {
 	fmt.Scanf("%s\n", &input)
 	fmt.Scanf("%d\n", &delta)
 
-	fmt.Printf("%d\n", length)
-	fmt.Printf("%s\n", input)
-	fmt.Printf("%d\n", delta)
+	alphabetLower := "abcdefghijklmnopqrstuvwxyz"
+	alphabetUpper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ret := ""
+	for _, ch := range input {
+		switch {
+		case strings.IndexRune(alphabetLower, ch) >= 0:
+			ret = ret + string(rotate(ch, delta, []rune(alphabetLower)))
+		case strings.IndexRune(alphabetUpper, ch) >= 0:
+			ret = ret + string(rotate(ch, delta, []rune(alphabetUpper)))
+		default:
+			ret = ret + string(ch)
+		}
+	}
+	fmt.Println(ret)
+}
+
+func rotate(s rune, delta int, key []rune) rune {
+	idx := strings.IndexRune(string(key), s)
+	if idx < 0 {
+		panic("idx < 0")
+	}
+	idx = (idx + delta) % len(key)
+	return key[idx]
 }
