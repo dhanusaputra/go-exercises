a337f4b5104980d8384eb4d96ea32f0f053f6236
dhanu <andreasdhanu@gmail.com>
2020-02-27 08:18:18 +0700

    add task 2

--- task/cmd/add.go
+++ task/cmd/add.go
@@ -1,9 +1,21 @@
 package cmd
 
-var AddCmd = &cobra.Command{
-	Use:"add",
-	Short:"Add a task to your list",
-	Run: func(cmd *cobra.Command,args []string){
-		fmt.Println("add called")
-	}
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a task to your list",
+	Run: func(cmd *cobra.Command, args []string) {
+		task := strings.Join(args, " ")
+		fmt.Printf("Add \"%s\" to task list\n", task)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(addCmd)
 }
